a36d2b7da54740578fe1d112016df94770d37509
Herri Fransisca <herrifransisca@outlook.com>
2017-08-09 21:30:36 +0700

    Components.

---
+++ src/app/courses.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  template: `
+    <h1>Courses</h1>
+  `
+})
+export class CoursesComponent {
+
+}
+
+// note: selector: 'courses'  <courses>
+// note: selector: '.courses' <div class="courses">
+// note: selector: '#courses' <div id="courses">
