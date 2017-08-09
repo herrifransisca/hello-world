11f03f9b5e95acb7fd98be01ba5189b4fb280c64
Herri Fransisca <herrifransisca@outlook.com>
2017-08-09 22:44:18 +0700

    Adding Bootstrap.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <img [src]="imageUrl" />                  // using property binding
-    <table>
-      <tr>
-        // <td [colspan]="colSpan"></td>      // Error, because "td" doesn't have a PROPERTY called colspan
-        // solution:
-        <td [attr.colspan]="colSpan"></td>   // using attribute binding
-      </tr>
-    </table>
-
-    // note: "property binding" uses "PROPERTY of DOM"
-    //       "attribute binding" uses "ATTRIBUTE of HTML"
+    <button class="btn btn-primary">Save</button>
   `
 })
 export class CoursesComponent {
-  imageUrl = '';
-  colSpan = 2;
 }
