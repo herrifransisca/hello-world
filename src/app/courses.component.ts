3b546d7d049d70d4e536c071f6da149655dfd974
Herri Fransisca <herrifransisca@outlook.com>
2017-08-10 09:42:32 +0700

    Template Variables, how to use by using #email, (email.value).

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <input (keyup.enter)="onKeyUp($event)" />
+    <input #email (keyup.enter)="onKeyUp(email.value)" />
   `
 })
 export class CoursesComponent {
-  onKeyUp($event) {
-    console.log($event.target.value);
+  onKeyUp(email) {
+    console.log(email);
   }
 }
