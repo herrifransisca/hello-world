3a932a83181851c5776738c968865f4a37827c61
Herri Fransisca <herrifransisca@outlook.com>
2017-08-10 11:01:04 +0700

    Assignment 2 - Favorite Component.

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+})
+export class FavoriteComponent {
+  isFavorite: boolean;
+
+  onClick() {
+    this.isFavorite = !this.isFavorite;
+  }
+}
