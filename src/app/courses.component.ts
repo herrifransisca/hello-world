469490b5e62c3c388f9cc105c57b96111ae4f82a
Herri Fransisca <herrifransisca@outlook.com>
2017-08-10 09:39:25 +0700

    Template Variables.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <input (keyup.enter)="onKeyUp()" />
+    <input (keyup.enter)="onKeyUp($event)" />
   `
 })
 export class CoursesComponent {
-  onKeyUp() {
-    console.log('ENTER was pressed');
+  onKeyUp($event) {
+    console.log($event.target.value);
   }
 }
