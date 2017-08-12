3c1f548881f4bc028fadef5c9367897e3a434b8c
Herri Fransisca <herrifransisca@outlook.com>
2017-08-12 19:01:04 +0700

    Apply Styles (1st way) (External).

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
-  templateUrl: './favorite.component.html'
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent {
   @Input('is-favorite') isFavorite: boolean;
