57a55d328eb9f7b30c5680b7be5d26942d6d7c9b
Herri Fransisca <herrifransisca@outlook.com>
2017-08-12 15:52:22 +0700

    Aliasing Input Properties.

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
-  templateUrl: './favorite.component.html',
-  inputs: ['isFavorite']
+  templateUrl: './favorite.component.html'
 })
 export class FavoriteComponent {
-  isFavorite: boolean;
+  @Input('is-favorite') isFavorite: boolean;
+  // here's the lesson: 
+  //    if you're building a reusable components, 
+  //    give your input properties an alias to keep the contract of your component stable.
 
   onClick() {
     this.isFavorite = !this.isFavorite;
