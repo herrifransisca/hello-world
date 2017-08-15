2d59fd50e3df828835f02b87912577371a11d5e5
Herri Fransisca <herrifransisca@outlook.com>
2017-08-15 21:34:15 +0700

    70. ngStyle (Before Refactoring).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
-    { id: 1, name: 'Course 1' },
-    { id: 1, name: 'Course 2' },
-    { id: 1, name: 'Course 3' }
-  ];
+  canSave = true;
 }
