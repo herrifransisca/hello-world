594836f6073aea9e0f56f81a74fd717a3cc93641
Herri Fransisca <herrifransisca@outlook.com>
2017-08-15 15:57:48 +0700

    Reset / clear app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +6,4 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  post = {
-    title: 'Title',
-    isFavorite: true
-  };
-
-  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
-    console.log('Favorite changed: ', eventArgs);
-  }
 }
-
-// mosh recommendation is to use template externally if there more than 5 lines of code
-// choose either "templateUrl or template"
