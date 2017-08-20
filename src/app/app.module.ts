406deff92de6e68bd7f6bfa6bcdd5f7278781aed
Herri Fransisca <herrifransisca@outlook.com>
2017-08-20 17:06:53 +0700

    Assignment 6: Course Form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { LikeComponent } from './like/like.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     InputFormatDirective,
     ZippyComponent,
     LikeComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+  categories = [
+    { id: 1, name: 'Developmet' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' }
+  ];
+
+  submit(course) {
+    console.log(course);
+  }
+}
