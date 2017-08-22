1d1344ef2f61d25fdf38748764e2a7472dcb3a22
Herri Fransisca <herrifransisca@outlook.com>
2017-08-22 20:40:57 +0700

    Adding validation (cleaner way).

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -11,4 +11,8 @@ export class SignupFormComponent {
     username: new FormControl('', Validators.required),
     password: new FormControl()
   });
+
+  get username() {
+    return this.form.get('username');
+  }
 }
