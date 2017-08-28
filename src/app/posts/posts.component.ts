016ba6b00502eb0f3633fb8a462ebe806eb95ed2
Herri Fransisca <herrifransisca@outlook.com>
2017-08-28 16:13:56 +0700

    105. Creating data.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,14 +8,23 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent {
   posts: any[];
-  url = 'https://jsonplaceholder.typicode.com/posts';
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http) {
+  // constructor(http: Http) {    -> http only accessible in the constructor, use "private" so it can be used outside constructor
+  constructor(private http: Http) {
     http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
       });
   }
 
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
 
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+      });
+  }
 }
