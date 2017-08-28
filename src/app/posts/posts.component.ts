d90fa436a6e5ba7d7db4c001bc2d471b32668825
Herri Fransisca <herrifransisca@outlook.com>
2017-08-28 20:39:12 +0700

    114. Throwing Application-specific errors.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,3 +1,5 @@
+import { AppError } from './../common/app-error';
+import { NotFoundError } from './../common/not-found-error';
 import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -63,8 +65,8 @@ export class PostsComponent implements OnInit {
           let index = this.posts.indexOf(post);
           this.posts.splice(index, 1);
         },
-        (error: Response) => {
-          if (error.status === 404)
+        (error: AppError) => {
+          if (error instanceof NotFoundError)
             alert('This post has already been deleted.');
           else {
             alert('An unexpected error occurred.');
